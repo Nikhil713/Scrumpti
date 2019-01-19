b0b832fde8b2c127cc69f998b74d75a673d4bfae
Nikhil713 <nikhilbabu213@gmail.com>
2019-01-19 18:45:18 +0530

    Add SignIn and SignUp option

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -8,37 +8,49 @@ class Navbar extends Component {
         e.preventDefault();
     }
 
-    // handleMouseOver(e){
-    //     this.setState{
-            
-    //     }
-    // }
+    handleSignIn(e){
+
+    }
+
+    handleSignUp(e){
+        
+    }
+
     render() {
       return (
         <div className="Navbar">
-        <nav className = "navbar">
-        <div className = "appName">
-            <a href = "#" >Food-App</a>
-        </div>
-        <div className = "navLinks">
-            <a href = "#">Our Story</a>
-            <a href = "#">Menus</a>
-            <a href = "#">Order Online</a>
-            <a href = "#">Catering</a>
-            <a href = "#">Events</a>
-            <a href = "#">Contact & Reservations</a>
-        </div>
-        <div className = "login" >
-            <form onSubmit={this.handleClick}>
-                <input type="email" class="username" placeholder="Username"/>
-                <input type="password" class="password" placeholder="Password"/>
-                <button className = "loginbutton" onMouseOver={this.handleMouseOver}>Log In</button>
-            </form>
-            <div className = "signUp">
-                Don't have an account? <a href="#">Sign up</a>
+
+            <nav className = "navbar">
+            
+            <div className = "appName">
+                <a href = "#" >Food-App</a>
             </div>
-        </div>
-    </nav>
+
+            <div className = "navLinks">
+                <a href = "#">Our Story</a>
+                <a href = "#">Menus</a>
+                <a href = "#">Order Online</a>
+                <a href = "#">Catering</a>
+                <a href = "#">Events</a>
+                <a href = "#">Contact & Reservations</a>
+            </div>
+        
+            <div className = "login" >
+                <div class="searchBar">
+                    <input type = "text" placeholder = "Search FoodApp" className = "searchBox"></input>
+                </div>
+                <form onSubmit={this.handleClick}>
+                    {/* <input type="email" class="username" placeholder="Username"/>
+                    <input type="password" class="password" placeholder="Password"/> */}
+                    <button className = "signInButton" onClick={this.handleSignIn}>Sign In</button>
+                    <button className = "signUpButton" onClick={this.handleSignUp}>Sign Up</button>
+                </form>
+                {/* <div className = "signUp">
+                    Don't have an account? <a href="#">Sign up</a>
+                    </div> */}
+            </div>
+            
+            </nav>
   
         </div>
       );
